a0c1108ab0d6c25b81c269cdac0409c068be25c4
Ana Paula Nolêto <ana.noleto@rarolabs.com.br>
2024-01-17 10:33:14 -0300

    feat: start about

---
+++ main.js
@@ -0,0 +1,6 @@
+// Navigation ar effects scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
